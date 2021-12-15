46b6818c43f47a7062d94ec9ac07d98109d214ee
Vitor Araújo <j.biel.ca15@gmail.com>
2021-12-15 19:32:13 -0300

    update db connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://njugywayokzlnz:3387b764f10d70e3b38dc6a580e4b9e10c3e8ac417968b49c6ecbd7e7b916820@ec2-44-199-86-61.compute-1.amazonaws.com:5432/d4pbfvsu475cus');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
