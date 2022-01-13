773da6510d09ae1b353845d1f7e4c6d6f6099854
Vitor Araújo <j.biel.ca15@gmail.com>
2022-01-12 22:37:56 -0300

    asdsadasdsa

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -17,24 +17,28 @@
         <div id="cards-container" class="row">
         @foreach($envio as $e) <!--"$e" é a versão foreach de $envios-->
             @if($e->user_id==$userEmail['id'])
-                <div class="card-body">
-                <a href="/upload/{{$e->id}}" target="_blank"><p><img src="/img/arquivo.png" alt= "arquivo" width="100"></p></a>
-
+            <div class="col card-body">
+                <!--<a href="https://if-estudos-bucket.s3.sa-east-1.amazonaws.com/arquivos/{{$e->file}}" target="_blank"><p><img src="/img/arquivo.png" alt= "arquivo"></p></a>-->
+                <a href="/upload/{{$e->id}}" target="_blank"><p><img src="/img/arquivo.png" alt= "arquivo"></p></a>
                     @if($e->report=="1")
                     <p class="card-name" title="Envio reportado"><b>{{$e->nome_envio}}</b> <i class="fa fa-exclamation" aria-hidden="true" style="color: red"></i></p>
                     @else
                     <p class="card-name"><b>{{$e->nome_envio}}</b></p>
                     @endif
-                    <p>Autor: {{$e->nome_usuario}}</p>
-                    <p class="card-date">Data:{{$e->updated_at->format('d/m/Y')}}</p>
+                    <p>Autor: <a href="/perfil/{{$e->id}}" style="color: black">{{$e->nome_usuario}}</a></p>
+                    <p class="card-date">Data: {{$e->updated_at->format('d/m/Y')}}</p>
                     <p>Disciplina: {{$e->disciplina}}</p>
-                    <p>Descrição: {{$e->descricao}}</p>
+                    <div class="hidden"><p>Descrição: {{$e->descricao}}</p></div>
                     <a href="/upload/{{$e->id}}" target="_blank">
+
                     <div id="buttons">
-                    <button class="btn btn-success" title="Baixar Envio"><i class="fa fa-download"></i> Baixar Envio</button></a> 
-                    <a href="/reportar/{{$e->id}}"><i class="fa fa-exclamation-circle" aria-hidden="true" onclick="alerta()" title="Reportar envio" style="font-size: large; color: red"></i></a>
-                    </div>  
-                </div>
+                    <button class="btn btn-success envios" title="Baixar Envio"><i class="fa fa-download"></i> Baixar Envio</button></a> 
+                    <a href="/reportar/{{$e->id}}"><i class="fa fa-exclamation-circle" aria-hidden="true" onclick="alerta()" title="Reportar envio - reporte ao administrador do site se você achar que o conteúdo não é adequado para publicação" style="font-size: large; color: red;"></i></a>
+
+
+                    </div>                    
+                    
+                </div><!--CARD BODY-->
             @endif
         @endforeach
 
